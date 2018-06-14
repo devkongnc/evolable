11b0b74ed27be9c0d2ffb5c6c60c028e31f49bf9
= <=>
2018-06-14 20:57:18 +0700

    t

--- wp-content/themes/kyushoku/sidebar.php
+++ wp-content/themes/kyushoku/sidebar.php
@@ -22,48 +22,16 @@
                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
                                    <i class="fa fa-bars"></i>
                                    </button>
-                                   <a class="navbar-brand" href="index.html">
-                                   <span ><img src="<?php echo get_template_directory_uri(); ?>/new/img/logo.png" ></span>
-                                   </a>
+                                   <?php switch_to_blog(1); theme_custom_logo(); restore_current_blog();?>
                                 </div>
-                <ul class="lang-menu">
-                  <li><a href="#" class="current">JP</a></li>
-                  <li><a href="#">EN</a></li>
-                  <li><a href="#">VN</a></li>
-                </ul>
+                <?php  
+     if (function_exists('qtrans_generateLanguageSelectCode')) {
+        //print_r(qtrans_generateLanguageSelectCode('hreflang'));
+          echo qtrans_generateLanguageSelectCode('short');
+      } 
+    ?>
                                 <div class="collapse navbar-collapse navbar-main-collapse">
-                                   <ul class="nav navbar-nav" id="top-nav">
-                                      <li class="current">
-                                         <a href="#">トップ</a>
-                                      </li>
-                                    
-                                      <li>
-                                         <a href="column.html">求職者応援コラム</a>
-                                      </li>
-
-                                       <li  class="nav-parrent">
-                                           <a href="#">求人情報検索</a>
-                                           <ul class="sub-nav">
-                                            <li><a href="search.html">求人情報一覧</a></li>
-                                            <li><a href="blog.html">ニュース・ブログ</a></li>
-                                            <li><a href="service.html">利用者インタビュー</a></li>
-                                            <li><a href="contact-form.html">応募フォーム</a></li>
-                                           </ul>
-                                       </li>
-                                       <li>
-                                           <a href="about.html">企業概要</a>
-                                       </li>
-                                       <li>
-                                          <ul class="lang-menu">
-                        <li><a href="#" class="current">JP</a></li>
-                        <li><a href="#">EN</a></li>
-                        <li><a href="#">VN</a></li>
-                      </ul>
-                                       </li>
-                                       
-                                      
-
-                                   </ul>
+                                   <?php get_template_part( 'template-parts/navigation/navigation', 'leftmenu' ); ?>
                                 </div>
 
                             <a href="#" class="employ-btn"><img src="<?php echo get_template_directory_uri(); ?>/new/img/icon-building.png" > 採用企業の皆さまへ</a>
