2c02b6571e0cad92d30f19df2294c24db196a6ab
Kong Nguyen <dev.kongnc@gmail.com>
2018-06-16 17:42:46 +0700

    eaa-kong update search top page

--- wp-content/themes/kigyo/column-index.php
+++ wp-content/themes/kigyo/column-index.php
@@ -1,5 +1,4 @@
-
-<?php 
+<?php
 /**
  * The front page template file
  *
@@ -21,9 +20,9 @@ get_sidebar();
     <div class="breadcrumb-bg">
         <h2><?php if (qtrans_getLanguage()=='ja'){ ?>
             News Release & Column
-        <?php }else if (qtrans_getLanguage()=='vi'){ ?> 
+        <?php }else if (qtrans_getLanguage()=='vi'){ ?>
             News Release & Column
-        <?php }else if (qtrans_getLanguage()=='en'){ ?> 
+        <?php }else if (qtrans_getLanguage()=='en'){ ?>
             News Release & Column
         <?php } ?>
     </h2>
@@ -33,9 +32,9 @@ get_sidebar();
         <div class="bc-desc">
         <?php if (qtrans_getLanguage()=='ja'){ ?>
             近年、日本国内で働く外国人や、逆に国外で働く日本人の姿をよく見かけるようになりました。そんな背景などを盛り込みながらエボラブルアジアエージェントの概要を200文字くらいで説明した文章を入れようと思います。近年、日本国内で働く外国人や、逆に国外で働く日本人の姿をよく見かけるように
-        <?php }else if (qtrans_getLanguage()=='vi'){ ?> 
+        <?php }else if (qtrans_getLanguage()=='vi'){ ?>
             近年、日本国内で働く外国人や、逆に国外で働く日本人の姿をよく見かけるようになりました。そんな背景などを盛り込みながらエボラブルアジアエージェントの概要を200文字くらいで説明した文章を入れようと思います。近年、日本国内で働く外国人や、逆に国外で働く日本人の姿をよく見かけるように
-        <?php }else if (qtrans_getLanguage()=='en'){ ?> 
+        <?php }else if (qtrans_getLanguage()=='en'){ ?>
             近年、日本国内で働く外国人や、逆に国外で働く日本人の姿をよく見かけるようになりました。そんな背景などを盛り込みながらエボラブルアジアエージェントの概要を200文字くらいで説明した文章を入れようと思います。近年、日本国内で働く外国人や、逆に国外で働く日本人の姿をよく見かけるように
         <?php } ?>
     </div>
@@ -43,7 +42,7 @@ get_sidebar();
         </div>
         </div>
     </div>
-    
+
     <div class="news-top">
         <div class="container">
         <div clss="row">
@@ -52,24 +51,24 @@ get_sidebar();
             <h2 class="title-h">
             <?php if (qtrans_getLanguage()=='ja'){ ?>
                 News Release
-            <?php }else if (qtrans_getLanguage()=='vi'){ ?> 
+            <?php }else if (qtrans_getLanguage()=='vi'){ ?>
                 News Release
-            <?php }else if (qtrans_getLanguage()=='en'){ ?> 
+            <?php }else if (qtrans_getLanguage()=='en'){ ?>
                 News Release
             <?php } ?>
     </h2>
             <?php if (qtrans_getLanguage()=='ja'){ ?>
                 <a href="<?php echo get_site_url(); ?>/ja/newsrelease" class="view-more-btn">一覧を見る</a>
-            <?php }else if (qtrans_getLanguage()=='vi'){ ?> 
+            <?php }else if (qtrans_getLanguage()=='vi'){ ?>
                 <a href="<?php echo get_site_url(); ?>/vi/newsrelease" class="view-more-btn">一覧を見る</a>
-            <?php }else if (qtrans_getLanguage()=='en'){ ?> 
+            <?php }else if (qtrans_getLanguage()=='en'){ ?>
                <a href="<?php echo get_site_url(); ?>/en/newsrelease" class="view-more-btn">一覧を見る</a>
             <?php } ?>
 
         </div>
         <?php
         global $post;
- 
+
     $posts = get_posts( array(
         'posts_per_page' => 3,
         'post_type' => 'newsrelease',
@@ -81,35 +80,35 @@ get_sidebar();
     if ($posts) { ?>
  <ul class="news-list">
     <?php
-        foreach ($posts as $post) : 
+        foreach ($posts as $post) :
             setup_postdata($post); ?>
-            
-            
+
+
             <li>
                 <label class="news-date"><?php echo get_the_date('Y.m.d'); ?></label>
                 <ul class="categories-list">
-                    <?php 
-                        foreach((get_the_category()) as $key => $category) { 
+                    <?php
+                        foreach((get_the_category()) as $key => $category) {
                             $category_link = get_category_link($category->cat_ID); ?>
                             <li><a class="<?php echo ($key%2) ? "blue" : "pink"; ?>" href="<?php echo $category_link; ?>"><?php echo $category->cat_name; ?></a></li>
-                            
+
                     <?php } ?>
                 </ul>
                 <a href="<?php echo get_permalink(); ?>" class="news-title"><?php the_title(); ?></a>
             </li>
-   
+
         <?php
         endforeach;
         wp_reset_postdata();
         echo '</ul>';
     }
-?>         
+?>
         </div>
         </div>
         </div>
     </div>
-  
-   
+
+
    <div class="blog-section gray-bg">
         <div class="container">
         <div clss="row">
@@ -123,7 +122,7 @@ get_sidebar();
         <div clss="row">
             <div class="col-md-12">
                 <div class="blog-blk">
-                
+
                         <img src="<?php echo get_template_directory_uri(); ?>/new/img/img0.jpg" >
                         <a href="#">【経験必須】トレーニングマネージャー募集</a>
                         <ul class="tags-list">
@@ -138,10 +137,10 @@ get_sidebar();
                             ■業務内容・日本国内の代理店様への電話対応・納期調整や仕様変更を関係部署との調整 ■必須スキル・社会人経験3年以上(VISA取得の為)・英語での実務経験(日常英会話程度でも可)…
                         </p>
                         <span class="blog-date">― 2018.04.01 UPDATED</span>
-                    
+
                 </div>
                 <div class="blog-blk">
-                
+
                         <img src="<?php echo get_template_directory_uri(); ?>/new/img/img0.jpg" >
                         <a href="#">【経験必須】トレーニングマネージャー募集</a>
                         <ul class="tags-list">
@@ -156,10 +155,10 @@ get_sidebar();
                             ■業務内容・日本国内の代理店様への電話対応・納期調整や仕様変更を関係部署との調整 ■必須スキル・社会人経験3年以上(VISA取得の為)・英語での実務経験(日常英会話程度でも可)…
                         </p>
                         <span class="blog-date">― 2018.04.01 UPDATED</span>
-                
+
                 </div>
                 <div class="blog-blk">
-                    
+
                         <img src="<?php echo get_template_directory_uri(); ?>/new/img/img0.jpg" >
                         <a href="#">【経験必須】トレーニングマネージャー募集</a>
                         <ul class="tags-list">
@@ -174,13 +173,13 @@ get_sidebar();
                             ■業務内容・日本国内の代理店様への電話対応・納期調整や仕様変更を関係部署との調整 ■必須スキル・社会人経験3年以上(VISA取得の為)・英語での実務経験(日常英会話程度でも可)…
                         </p>
                         <span class="blog-date">― 2018.04.01 UPDATED</span>
-                    
+
                 </div>
             </div>
         </div>
         </div>
    </div>
-   
+
     <div class="blog-section">
         <div class="container">
         <div clss="row">
@@ -189,17 +188,17 @@ get_sidebar();
                 <h2 class="title-h">
                 <?php if (qtrans_getLanguage()=='ja'){ ?>
                     企業様向けコラム
-                <?php }else if (qtrans_getLanguage()=='vi'){ ?> 
+                <?php }else if (qtrans_getLanguage()=='vi'){ ?>
                     企業様向けコラム
-                <?php }else if (qtrans_getLanguage()=='en'){ ?> 
+                <?php }else if (qtrans_getLanguage()=='en'){ ?>
                    企業様向けコラム
                 <?php } ?>
             </h2>
                 <?php if (qtrans_getLanguage()=='ja'){ ?>
                     <a href="<?php echo get_site_url(); ?>/ja/column" class="view-more-btn">一覧を見る</a>
-                <?php }else if (qtrans_getLanguage()=='vi'){ ?> 
+                <?php }else if (qtrans_getLanguage()=='vi'){ ?>
                     <a href="<?php echo get_site_url(); ?>/vi/column" class="view-more-btn">一覧を見る</a>
-                <?php }else if (qtrans_getLanguage()=='en'){ ?> 
+                <?php }else if (qtrans_getLanguage()=='en'){ ?>
                    <a href="<?php echo get_site_url(); ?>/en/column" class="view-more-btn">一覧を見る</a>
                 <?php } ?>
                 </div>
@@ -207,10 +206,10 @@ get_sidebar();
         </div>
         <div clss="row">
             <div class="col-md-12">
-            
+
                 <?php
                 global $post;
-             
+
                 $posts = get_posts( array(
                     'posts_per_page' => 2,
                     'post_type' => 'columns',
@@ -218,41 +217,41 @@ get_sidebar();
                     'orderby' => 'post_date',
                     'order' => 'DESC',
                 ) );
-             
+
                 if ($posts) {
-                    foreach ($posts as $post) : 
+                    foreach ($posts as $post) :
                         setup_postdata($post); ?>
 
                         <div class="blog-blk">
-                
+
                         <img src="<?php echo get_the_post_thumbnail_url( null, 'post-thumbnail' );?>" >
 
                         <a href="<?php echo get_permalink($post->ID); ?>"><?php the_title(); ?></a>
-                        
+
                         <ul class="tags-list">
-                            <?php 
-                        foreach((get_the_category()) as $key => $category) { 
+                            <?php
+                        foreach((get_the_category()) as $key => $category) {
                             $category_link = get_category_link($category->cat_ID); ?>
-                            
+
                             <li>
                                 <a href="<?php echo $category_link; ?>" class="catlink" data-id="<?php echo $category->cat_ID; ?>"><?php echo $category->cat_name; ?></a>
                     </li>
-                            
-                    <?php   } 
+
+                    <?php   }
                     ?>
 
-                           
+
                         </ul>
                         <p>
                         <?php echo mb_strimwidth(wp_strip_all_tags(get_the_content($post->post_content)), 0, 150, '...'); ?>
-                           
+
                         </p>
                         <span class="blog-date">― <?php the_modified_time('Y.m.d'); ?> UPDATED</span>
-                
+
                 </div>
 
 
-                        
+
                     <?php
                     endforeach;
                     wp_reset_postdata();
@@ -262,7 +261,7 @@ get_sidebar();
         </div>
         </div>
    </div>
-   
+
     <div class="blog-section gray-bg">
         <div class="container">
         <div clss="row">
@@ -275,9 +274,9 @@ get_sidebar();
         </div>
         <div clss="row">
             <div class="col-md-12">
-            
+
                 <div class="blog-blk">
-                
+
                         <img src="<?php echo get_template_directory_uri(); ?>/new/img/img0.jpg" >
                         <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing</a>
                         <ul class="tags-list">
@@ -289,13 +288,13 @@ get_sidebar();
                             <li><a href="#">マネジメント</a></li>
                         </ul>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
                         </p>
                         <span class="blog-date">― 2018.04.01 UPDATED</span>
-                
+
                 </div>
                 <div class="blog-blk">
-                    
+
                         <img src="<?php echo get_template_directory_uri(); ?>/new/img/img0.jpg" >
                         <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing</a>
                         <ul class="tags-list">
@@ -307,17 +306,17 @@ get_sidebar();
                             <li><a href="#">マネジメント</a></li>
                         </ul>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
                         </p>
                         <span class="blog-date">― 2018.04.01 UPDATED</span>
-                    
+
                 </div>
             </div>
         </div>
         </div>
-   </div> 
-   
-   
+   </div>
+
+
    <div class="recommend-job">
     <div class="container">
         <div class="row">
@@ -325,9 +324,9 @@ get_sidebar();
                 <h2>
                 <?php if (qtrans_getLanguage()=='ja'){ ?>
                     おすすめの求人
-                <?php }else if (qtrans_getLanguage()=='vi'){ ?> 
+                <?php }else if (qtrans_getLanguage()=='vi'){ ?>
                     おすすめの求人
-                <?php }else if (qtrans_getLanguage()=='en'){ ?> 
+                <?php }else if (qtrans_getLanguage()=='en'){ ?>
                     おすすめの求人
                 <?php } ?>
             </h2>
@@ -337,14 +336,14 @@ get_sidebar();
             <?php
             switch_to_blog(2);
                 global $post;
-             
+
                 $posts = get_posts( array(
                     'posts_per_page' => 8,
                     'post_type' => 'jobs',
                     'post_status'=>'publish',
                     'orderby'   => 'rand',
                 ) );
-             
+
                 if ($posts) {
                     $taxonomies = array('job-location');
                 $check_later = array();
@@ -361,10 +360,10 @@ get_sidebar();
                     $args       = array('hide_empty' => false);
                     $terms      = get_terms($taxonomies, $args );
                    // $i=4;
-                    foreach ($posts as $key => $post) : 
-                     
+                    foreach ($posts as $key => $post) :
 
-                        setup_postdata($post); 
+
+                        setup_postdata($post);
                         $term_positions = get_the_terms($post->ID, "job-position");
                         $term_locations = get_the_terms($post->ID, 'job-location');
                        // print_r($term_locations);
@@ -374,7 +373,7 @@ get_sidebar();
                         <div class="col-md-3 col-sm-6 recruit">
 
                 <div class="job-blk">
-                		<?php 
+                		<?php
                         	$custom = get_post_custom();
                         	$image = wp_get_attachment_image(get_post_meta($post->ID, 'default_image', true));
                         	if($image){
@@ -383,11 +382,11 @@ get_sidebar();
                         		echo '<img src="'.get_template_directory_uri().'/new/img/s2.jpg" >';
                         	}
                        	?>
-                    
+
                     <a href="<?php echo get_permalink($post->ID); ?>"><?php the_title(); ?></a>
                     <div class="job-if"><img src="<?php echo get_template_directory_uri(); ?>/new/img/icon-money.png" >
                         <?php
-                        
+
                        foreach ($custom['salary'] as $key => $value) {
                         //print_r($value);
                            //echo $value;
@@ -398,25 +397,25 @@ get_sidebar();
                         //echo mb_strimwidth(wp_strip_all_tags(the_field('salary',$post->ID)), 0, 5, '...');
                           //  endif; ?></div>
                     <div class="job-if loca"><img src="<?php echo get_template_directory_uri(); ?>/new/img/icon-map.png" >
-                        <?php 
+                        <?php
                             if($term_locations){
-                            foreach ($term_locations as $location) { 
+                            foreach ($term_locations as $location) {
                             echo $location->name . ", ";
                         } } ?>
                     </div>
                     <span class="job-date">― <?php the_modified_time('Y.m.d'); ?> UPDATE</span>
                 </div>
-            </div>  
+            </div>
 
                     <?php
-             
+
                     endforeach;
                     wp_reset_postdata();
-                   
+
 
 
                 }
-                
+
                 restore_current_blog();
             ?>
 
